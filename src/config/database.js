7c44a20fcbb8f6852ee11b545f28386aee4ce22d
dennis840 <avillacische@gmail.com>
2026-08-03 12:08:36 -0500

    Fix conexion BD Neon

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgresql://neondb_owner:npg_RSpe4FtMD9lZ@ep-delicate-bonus-ay5495dk-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require',
   ssl: {
     rejectUnauthorized: false,
   },
